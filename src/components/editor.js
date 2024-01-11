5ee30a846c7af9201fcd00bd82b4faae4fa48d6f
amitk-codes <amitkcodes@gmail.com>
2024-01-12 00:51:29 +0530

    added functionalities in editor

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, Modifier, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
-
+import "../stylesheets/editor.css"
+import { customMappedStyle, functionalityObj } from '../utils/constants';
+import { functionalityHandler } from '../utils/handlerFunctions';
 export const TextEditor = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const onChangeHandler = (newEditorState) => setEditorState(newEditorState)
 
+  const handleBeforeInput = (char, editorState) => {
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    if (char === ' ') {
+      const blockKey = selectionState.getStartKey();
+      const blockText = contentState.getBlockForKey(blockKey).getText();
+
+      const functionality = functionalityObj[blockText]
+
+      if (functionality) {
+        functionalityHandler(functionality, blockText, contentState, editorState, selectionState, setEditorState)
+        return 'handled';
+      }
+    }
+
+    return 'not-handled';
+  };
+
 
   return (
-    <div>
+    <div className='editor-div'>
       <Editor
         editorState={editorState}
         onChange={onChangeHandler}
+        handleBeforeInput={handleBeforeInput}
+        customStyleMap={customMappedStyle}
       />
     </div>
   );
